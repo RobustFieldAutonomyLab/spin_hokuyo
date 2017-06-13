05b8b61dfdd893d236dca0b3e40144209962dcd7
Sarah Bertussi <sbertuss@stevens.edu>
2017-06-13 12:55:42 -0400

    Subscribing Cloud Compilation Service

---
+++ tilting_lidar_clean/combine_clouds_subscriber.cpp
@@ -0,0 +1,146 @@
+/*********************************************************************
+* Software License Agreement (BSD License)
+*
+*  Copyright (c) 2008, Willow Garage, Inc.
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of the Willow Garage nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
+
+#include <cstdio>
+#include <ros/ros.h>
+
+// Services
+#include "laser_assembler/AssembleScans2.h"
+
+// Messages
+#include "sensor_msgs/PointCloud2.h"
+
+#include"dynamixel_msgs/JointState.h"
+#include"std_msgs/Time.h"
+#include<ros/time.h>
+/***
+ * This a simple test app that requests a point cloud from the
+ * point_cloud_assembler every 4 seconds, and then publishes the
+ * resulting data
+ */
+using namespace laser_assembler;
+
+ros::Time start;
+ros::Time end;
+int go = 0;
+
+void startTime(const std_msgs::Time &msg)
+{
+  start = msg.data;
+}
+
+void endTime(const std_msgs::Time &msg)
+{
+  end = msg.data;
+  go = 1;
+}
+
+class PeriodicSnapshotter
+{
+
+public:
+
+  PeriodicSnapshotter()
+  {
+    // Create a publisher for the clouds that we assemble
+    pub_ = n_.advertise<sensor_msgs::PointCloud2> ("assembled_cloud", 1);
+
+    // Create the service client for calling the assembler
+    client_ = n_.serviceClient<AssembleScans2>("assemble_scans2");
+
+  }
+
+  void compile()
+  {
+
+
+    // Populate our service request based on our timer callback times
+    AssembleScans2 srv;
+    srv.request.begin = start;
+    srv.request.end   = end;
+
+    // Make the service call
+    if (client_.call(srv))
+    {
+      ROS_INFO_STREAM("Published Cloud") ;
+      pub_.publish(srv.response.cloud);
+    }
+    else
+    {
+      ROS_ERROR("Error making service call\n") ;
+    }
+  }
+
+private:
+  ros::NodeHandle n_;
+  ros::Publisher pub_;
+  ros::ServiceClient client_;
+} ;
+
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "periodic_snapshotter");
+  ros::NodeHandle n;
+  ROS_INFO("Waiting for [build_cloud] to be advertised");
+  
+  //Wait for build cloud service to init
+  ros::service::waitForService("build_cloud");
+  
+  //Wait for dynamixel servo to init
+  ros::topic::waitForMessage<dynamixel_msgs::JointState>("/tilt_controller/state", ros::Duration(20));
+  
+  ROS_INFO_STREAM("Found build_cloud! Starting the snapshotter");
+  
+  ros::Subscriber sub_1=n.subscribe("/start_time", 1, &startTime);
+  ros::Subscriber sub_2=n.subscribe("/end_time", 1, &endTime);
+
+  PeriodicSnapshotter snapshotter;
+
+  while(ros::ok())
+  {
+    if(go==1)
+    {
+      snapshotter.compile();
+      go = 0;
+    }
+
+    else
+    {
+      ros::spinOnce();
+    }
+  }
+return 0;
+}
+
