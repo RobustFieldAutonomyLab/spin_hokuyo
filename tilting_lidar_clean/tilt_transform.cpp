e6a6156d4b225cf3afae91bf36fd9a6e8444dbc1
sbertussi <sbertuss@stevens.edu>
2017-06-14 13:49:31 -0400

    commenting updates

--- tilting_lidar_clean/tilt_transform.cpp
+++ tilting_lidar_clean/tilt_transform.cpp
@@ -3,13 +3,18 @@
 #include<dynamixel_msgs/JointState.h>
 
 //Module that applies transform to laser scan of tilting hokuyo laser
-
 using namespace std;
+
+//global variables
 float pos;
 
 //Recieves position values from dynamixel servo and uses angle to apply transform to laser scan
-void obtainValues(const dynamixel_msgs::JointState &msg) {
+void obtainValues(const dynamixel_msgs::JointState &msg)
+{
+    //gets position from message
     pos = msg.current_pos;
+    
+    //perform transform
     static tf::TransformBroadcaster br;
     tf::Transform transform;
     transform.setOrigin( tf::Vector3(0.0, 0.0, 0.0) );
@@ -19,11 +24,16 @@ void obtainValues(const dynamixel_msgs::JointState &msg) {
     br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "camera", "laser"));
 }
 
-int main(int argc, char **argv) {
+//main
+int main(int argc, char **argv)
+{
+    //initialize
     ros::init(argc, argv, "tilt_transform");
     ros::NodeHandle nh;
   
+    //subscirber to current position
     ros::Subscriber position_sub = nh.subscribe("/tilt_controller/state", 5, &obtainValues);
 
+    //wait for updates in position
     ros::spin();
 }
