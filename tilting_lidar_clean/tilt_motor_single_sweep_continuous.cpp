e673151c0cb0b572d39370258dd0d4f88365d125
Sarah Bertussi <sbertuss@stevens.edu>
2017-06-14 13:08:13 -0400

    launch files and intialize update

---
+++ tilting_lidar_clean/tilt_motor_single_sweep_continuous.cpp
@@ -0,0 +1,154 @@
+#include<ros/ros.h>
+#include<std_msgs/Float64.h>
+#include<dynamixel_msgs/JointState.h>
+#include<cmath>
+#include<laser_assembler/AssembleScans.h>
+#include<ros/time.h>
+#include<std_msgs/Time.h>
+#include<std_msgs/Empty.h>
+
+/*This code allows the motor to move back and forth to a maximum and minimum number of degrees defined in parameters.*/
+
+using namespace std;
+
+//global variables
+float error;
+int max_angle;
+int min_angle;
+float pause_time;
+
+//obtains error from message
+void obtainValues(const dynamixel_msgs::JointState &msg) 
+{
+    error = msg.error;
+}
+
+//creates all commands for the motor
+class Dynamixel
+{
+    private:
+    ros::NodeHandle nh;
+    ros::Publisher pub_1;
+    ros::Publisher pub_2;
+    ros::Publisher pub_3;
+    ros::Subscriber sub;
+
+    public:
+    Dynamixel();
+    void checkError();
+    void moveMotor(double position);
+    void startTime();
+    void endTime();
+};
+
+//creates publishers and subscriber
+Dynamixel::Dynamixel()
+{
+    pub_1 = nh.advertise<std_msgs::Float64>("/tilt_controller/command", 10);
+    pub_2 = nh.advertise<std_msgs::Time>("/time/start_time", 1);
+    pub_3 = nh.advertise<std_msgs::Time>("/time/end_time", 1);
+    sub   = nh.subscribe("/tilt_controller/state", 1, &obtainValues); //checks error
+}
+
+//creates message and publishes -> degree to radian to publish
+void Dynamixel::moveMotor(double position) {
+    double convert = (position * 3.14/180);
+    std_msgs::Float64 aux;
+    aux.data = convert;
+    pub_1.publish(aux);
+    ROS_INFO_STREAM(aux);
+}
+
+//ensures proper alignment
+void Dynamixel::checkError()
+{
+    ROS_WARN_STREAM("hi");
+    ros::spinOnce();
+    while((abs (error))>0.05) 
+    {
+     ROS_ERROR_STREAM("bye");
+     ros::Duration(.1).sleep();
+     ros::spinOnce();
+    }
+    ROS_WARN_STREAM("boo");
+}
+
+//publishes start time for cloud compiler
+void Dynamixel::startTime()
+{
+  std_msgs::Time msg;
+  msg.data = ros::Time::now();
+  pub_2.publish(msg);
+}
+
+//publishes end time for cloud compiler
+void Dynamixel::endTime()
+{
+  std_msgs::Time msg;
+  msg.data = ros::Time::now();
+  pub_3.publish(msg);
+}
+
+//initilazies motor to min angle
+void initialize()
+{
+    Dynamixel motor_1;
+
+    motor_1.moveMotor(min_angle);
+    ros::Duration(pause_time).sleep();
+    motor_1.checkError();
+    ros::Duration(pause_time).sleep();
+}
+
+//performs one sweep min -> max -> min
+void sweep()
+{
+    Dynamixel motor;
+
+    motor.startTime();
+    motor.moveMotor(max_angle);
+    ros::Duration(pause_time).sleep();
+    motor.checkError();
+    ros::Duration(pause_time).sleep();
+
+    motor.moveMotor(min_angle);
+    ros::Duration(pause_time).sleep();
+    motor.checkError();
+    motor.endTime();
+    ros::Duration(pause_time).sleep();
+    ROS_INFO("Finished One Sweep!");
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "Motor_Tilt");
+    ros::NodeHandle nh;
+
+    //variables
+    int max;
+    int min;
+    double pause;
+    Dynamixel motor;
+
+    nh.param("maximum", max, 92);
+    nh.param("minimum", min, -92);
+    nh.param("pause", pause, 0.1);
+
+    //transfer parameters to global variables
+    max_angle = max;
+    min_angle = min;
+    pause_time = pause;
+
+    //Wait for servo init
+    ros::topic::waitForMessage<dynamixel_msgs::JointState>("/tilt_controller/state", ros::Duration(100));
+
+    //pause to allow motor object to initializei and set to min_angle
+    ros::Duration(1).sleep();
+    initialize();
+    ROS_INFO_STREAM("ready");  
+
+    while(ros::ok())
+    {
+      sweep();
+    }
+}
