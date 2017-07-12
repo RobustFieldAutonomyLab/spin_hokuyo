49452a6752e35f75faf91c78a9538219e71e909b
sbertussi <sbertuss@stevens.edu>
2017-07-12 16:13:29 -0400

    commenting update

--- tutorials/templates/servo_shell.cpp
+++ tutorials/templates/servo_shell.cpp
@@ -1,11 +1,16 @@
 #include<ros/ros.h>
 #include<std_msgs/Float64.h>
 
-/* This code was based on the examples provided in Chap. 8 of "Effective Robotics Programming with ROS - Third Edition" by Anil Mahtani, Luis Sanchez, Enrique Fernandea, and Aaron Martinez.
+/* This code was based on the examples provided in Chap. 8 of "Effective Robotics Programming with ROS - Third Edition"
+by Anil Mahtani, Luis Sanchez, Enrique Fernandea, and Aaron Martinez.
+This is a shell for publishing servo movement to the /commands topic.  The /commands published here are taken into the
+commander node to be processed and then transmitted to the servo.  The basic_motors.launch file must be used prior
+to running any nodes created with this code.  Alternatively, this node can be included in a new launch file (copied and
+then modified from basic_motors.launch) and will work.
 
-This is a shell for publishing servo movement to the /commands topic.  The /commands published here are taken into the commander node to be processed and then transmitted to the servo.  The basic_motors.launch file must be used prior to running any nodes created with this code.  Alternatively, this node can be included in a new launch file (copied and then modified from basic_motors.launch) and will work.
-
-This shell creates a Pulbisher class to which motor commands can be prepared and then published to the /commands topic.  To use this publisher, use motor.move("int") in the main function.  Replace "int" with the value, in degrees, to which you want the motor to move.  Please see commander.cpp for additional information on movement.  */
+This shell creates a Pulbisher class to which motor commands can be prepared and then published to the /commands topic.
+To use this publisher, use motor.move("int") in the main function.  Replace "int" with the value, in degrees, to which you
+want the motor to move.  Please see commander.cpp for additional information on movement.  */
 
 using namespace std;
 
